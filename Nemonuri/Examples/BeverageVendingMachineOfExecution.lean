f1d64b4ce6d6e06b93f1ebe212112a3cce4a2fa8
nemonuri <nemonuri2@gmail.com>
2026-08-05 10:35:17 +0900

    증명을 더 간단하게 줄일 수 있을 것 같은데! - states_getElem_eq 말고 states_getElem_eq' 을 새로 정의하자...!

--- Nemonuri/Examples/BeverageVendingMachineOfExecution.lean
+++ Nemonuri/Examples/BeverageVendingMachineOfExecution.lean
@@ -32,7 +32,7 @@ def ϱ := 𝐸𝑥𝑒𝑐{ts}⸨ &(.pay) ─⌞.insert_coin⌟→ &(.select)─
                     ─⌞.insert_coin⌟→ &(.select)─⌞.τ⌟→ State.soda ⸩
 
 
-
+/-! Execution fragments `ρ₁` and `ϱ` are initial, but `ρ₂` is not. -/
 
 theorem ρ₁_initial (x: ρ₁) : (x: ts.ExecutionFragmentRaw).IsInitial := by
   revert x
@@ -52,43 +52,35 @@ theorem ρ₁_initial (x: ρ₁) : (x: ts.ExecutionFragmentRaw).IsInitial := by
     simpa using lm2
 
 
-/-
-
-  dsimp [ρ₁] at lm1
-  have lm2 := ExprRaw.EvalToSet.mem_imp_isExecutionFragment _ _ lm1
-  dsimp [ExecutionFragmentRaw.IsInitial]
-  exists lm2
-  dsimp [ExecutionFragment.IsInitial]
-  simp only [Set.mem_singleton_iff]
-  dsimp only [ExecutionFragment.state0, ExecutionFragment.states]
-  dsimp [ExprRaw.EvalToSet] at lm1
-  cases x
+theorem ϱ_initial (x: ϱ) : (x: ts.ExecutionFragmentRaw).IsInitial := by
+  revert x; simp only [Subtype.forall]; intro x lm1
+  dsimp [ϱ] at lm1;
+  simp only [ExprRaw.EvalToSet.mem_iff_mem] at lm1
+  refine lm1.isInitial_iff.mpr ?_
+  rcases x with ϱ | ρ
+  · have lm2 req := @lm1.pre_is_prefix.states_getElem_eq _ _ _ 0 req |> Eq.symm
+    simpa using lm2
   · refine absurd lm1 ?_
-    refine ExprRaw.Mem.not_finite_infinite ?_ ?_
+    refine ExprRaw.Mem.not_infinite_finite ?_ ?_
     · dsimp
-    · simp only [ExprRaw.stepL_eq_stepL']
-      conv => lhs; change (HasLabel.toLabel (_: ExprRaw ts): Sequence.Label)
-      simp only [ExprRaw.stepL'_preserves_seqLabel, HasLabel.Preserves.cancel]
+    · conv => lhs; change (@HasLabel.toLabel Sequence.Label _ _ (ExprRaw ts) _ _)
+      simp only [ExprRaw.stepL_eq_stepL', ExprRaw.stepL'_preserves_seqLabel, HasLabel.Preserves.cancel]
       rfl
-  · have lm3 req := @lm1.pre_is_prefix.states_getElem_eq ts _ _ 0 req
-    refine Eq.trans (lm3 ?_ |> Eq.symm) ?_
-    · simp
-    · simp
--/
+
+theorem not_ρ₂_initial (x: ρ₂) : ¬(x: ts.ExecutionFragmentRaw).IsInitial := by
+  revert x; simp only [Subtype.forall]; intro x lm1
+  dsimp [ρ₂] at lm1
+  simp only [ExprRaw.EvalToSet.mem_iff_mem] at lm1
+  refine lm1.isInitial_iff.not.mpr ?_
+  have lm2 req := @lm1.pre_is_prefix.states_getElem_eq _ _ _ 0 req
+  simp at lm2
+  intro cont; simp at cont
+  have lm3 := Eq.trans lm2 cont
+  simp at lm3
 
 
-      --have lm3 s a := HasLabel.LabelHom.coe_mk _ (@ExprRaw.stepL'_preserves_seqLabel ts s a)
-      --conv => lhs; arg 1; dsimp [ExprRaw.stepL]
 
 
-
-
-      --have lm3 := HasLabel.LabelHom.coe_mk _ ExprRaw.stepL_preserves_seqLabel
-
-  --have lm1 := x.property
-  --have lm2 :=
-  --dsimp [ExecutionFragment.IsInitial]
-  --simp only [Set.mem_singleton_iff]
 
 
 end Examples.Executions
