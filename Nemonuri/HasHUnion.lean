92ed2c97ec89d3e4cf29b1e2400d618fc267c3bd
nemonuri <nemonuri2@gmail.com>
2026-08-27 21:36:47 +0900

    중요한 건 이거네. 'HasHUnion' 의 'LeftType' 들의 'Universe' 는, 반드시 같아야 한다!

--- Nemonuri/HasHUnion.lean
+++ Nemonuri/HasHUnion.lean
@@ -37,9 +37,11 @@ instance toFunlike : FunLike (LiftableEmbedding T1 T2) T1 T2 where
 instance : EmbeddingLike (LiftableEmbedding T1 T2) T1 T2 where
   injective' x := x.toEmbedding.injective
 
+/-
 structure PartialLift (x: LiftableEmbedding T1 T2) where
   partialLift (t2: T2) : Option T1
   partialLift_valid (t2: T2) (t1: T1) : (partialLift t2 = some t1) ↔ (∃req, x.lift t2 req = t1)
+-/
 
 end LiftableEmbedding
 
@@ -57,9 +59,11 @@ inductive Label where
 
 universe u1 u2
 
+/-
 class HasPartialLift (T1 T2: Type u1) (T3: Type u2) [HasHUnion T1 T2 T3] where
   fst: LiftableEmbedding.PartialLift (@HasHUnion.fst T1 T2 T3 _)
   snd: LiftableEmbedding.PartialLift (@HasHUnion.snd T1 T2 T3 _)
+-/
 
 section TypeAbbrev
 
@@ -173,6 +177,13 @@ def hunionSet (s1: Set T1) (s2: Set T2) : Set T3 := (s1.image (embedAt T1 T2 .fs
 
 def hunionFinset [DecidableEq T3] (s1: Finset T1) (s2: Finset T2) : Finset T3 := (s1.image (embedAt T1 T2 .fst)) ∪ (s2.image (embedAt T1 T2 .snd))
 
+
+theorem hunionFinset_dist [DecidableEq T3] {s1: Finset T1} {s2: Finset T2}
+  : (hunionFinset s1 s2: Set T3) = (hunionSet (s1: Set T1) (s2: Set T2)) := by
+  dsimp [hunionFinset, hunionSet]
+  simp
+
+
 def hunionIndicator [∀lb rv, Decidable (rv ∈ EmbedRangeAt T1 T2 lb)] (s1: T1 → Bool) (s2: T2 → Bool) (rv: T3) : Bool :=
   let loc2 (_: Unit) : Bool :=
     if lm2: decide (rv ∈ EmbedRangeAt T1 T2 .snd) then
@@ -194,153 +205,36 @@ theorem hunionIndicator_eq_true_iff [∀lb rv, Decidable (rv ∈ EmbedRangeAt T1
   <;> cases lm2: decide (rv ∈ EmbedRangeAt T1 T2 .snd)
   <;> (simp at lm1 lm2; simp [lm1, lm2, hunionIndicator])
 
-#print hunionIndicator_eq_true_iff
-
-/-
-  revert lm1 lm2
-  simp [EmbedRangeAt, LeftTypeAt]
-  intro lv1 lm1 lv2 lm2
+theorem hunionIndicator_set_eq [∀lb rv, Decidable (rv ∈ EmbedRangeAt T1 T2 lb)] {s1: T1 → Bool} {s2: T2 → Bool}
+  : { t3 | hunionIndicator s1 s2 t3 = .true } = (hunionSet { t1 | s1 t1 = .true } { t2 | s2 t2 = .true }) := by
+  simp [hunionSet, Set.ext_iff]
+  intro rv
+  simp [hunionIndicator_eq_true_iff, EmbedRangeAt, LeftTypeAt]
   constructor
-  · rintro ⟨lm3, lm4⟩
-    exact Or.inl lm3
-  · intro lm3
-    rcases lm3 with lm3 | lm3
-    <;> simp [lm3]
--/
-  --simp [EmbedRangeAt] at lm1 lm2
-/-
-  · intro lm1
-    dsimp [hunionIndicator] at lm1
-    split at lm1 <;> (rename_i lm2)
-    · revert lm2; simp
-      intro lm2 lm1
-      refine Or.inl ?_
-      exists lm2
-    · revert lm2; simp
-      simp at lm1
-      rcases lm1 with ⟨lm1_1, lm1_2⟩
-      intro lm2
-      refine Or.inr ?_
-      exists lm1_1
-  · intro lm1
-    rcases lm1 with lm1 | lm1
-    <;> (rcases lm1 with ⟨lm1_1, lm1_2⟩)
-    · dsimp [hunionIndicator]
-      simp [lm1_1]
-      exact lm1_2
-    · dsimp [hunionIndicator]
-      simp
-      split <;> rename_i lm2
-      · revert lm1_1 lm2
-        simp [EmbedRangeAt, LeftTypeAt]
-        intro lv2 lm1 lm2 lv1 lm3
-        have lm4 :=
--/
-
-
-/-
-  refine not_iff_not.mp ?_
-  simp
-  constructor
-  · intro lm1
-    simp [hunionIndicator] at lm1
--/
-
-/-
-  Bool.casesOn (motive := fun _ => Bool)
-    (decide (rv ∈ EmbedRangeAt T1 T2 .fst))
-    (Bool.casesOn (motive := fun _ => Bool)
-      (decide (rv ∈ EmbedRangeAt T1 T2 .snd))
-      (.false)
-      )
--/
-
-/-
-def hunionIndicator [HasPartialLift T1 T2 T3] (s1: T1 → Bool) (s2: T2 → Bool) (x: T3) : Bool :=
-  match liftAt? T2 x with
-  | .some t1 => s1 t1
-  | .none =>
-  match liftAt? T1 x with
-  | .some t2 => s2 t2
-  | .none => .false
--/
-
-/-
-theorem hunionIndicator_eq_true_iff [HasPartialLift T1 T2 T3] {s1: T1 → Bool} {s2: T2 → Bool} {t3: T3}
-  : (hunionIndicator s1 s2 t3 = .true) ↔ ((∃req1, ∃(t1: T1), liftAt T2 t3 req1 = t1 ∧ (s1 t1 = .true) ) ∨ (∃req2, ∃(t2: T2), liftAt T1 t3 req2 = t2 ∧ (s2 t2 = .true))) := by
-  constructor
-  · intro lm1
-    dsimp [hunionIndicator] at lm1
-    split at lm1
-    · rename_i t1_2 lm2
-      refine Or.inl ?_
-      rewrite [liftAt?_eq_some_iff] at lm2
-      rcases lm2 with ⟨lm2_1, lm2_2⟩
-      exists lm2_1
-      exists t1_2
-    · split at lm1 <;> try simp at lm1
-      rename_i t1_2 lm2
-      refine Or.inr ?_
-      rewrite [liftAt?_eq_some_iff] at lm2
-      rcases lm2 with ⟨lm2_1, lm2_2⟩
-      exists lm2_1
-      exists t1_2
   · intro lm1
     rcases lm1 with lm1 | lm1
     <;> (
-      rcases lm1 with ⟨lm1, t1, lm2, lm3⟩
-      revert lm1
-      simp
-      intro x lm1 lm2
-      replace lm1_1 := lm1.symm
-      subst lm1_1
+      rcases lm1 with ⟨⟨lv, lm1⟩, lm2⟩
+      have lm3 := lm1.symm
+      subst lm3
       rewrite [embedAt_liftAt_eq] at lm2
-      subst lm2
       clear lm1 )
-    · dsimp [hunionIndicator]
-      split <;> (rename_i lm2)
-      · rewrite [liftAt?_eq_some_iff] at lm2
-        rcases lm2 with ⟨lm2_1, lm2_2⟩
-        rewrite [embedAt_liftAt_eq] at lm2_2
-        subst lm2_2
-        exact lm3
-      · rewrite [liftAt?_eq_none_iff] at lm2
-        simp at lm2
-    · dsimp [hunionIndicator]
-      split <;> (rename_i lm2)
-      · rename_i t2
-        rewrite [liftAt?_eq_some_iff] at lm2
-        rcases lm2 with ⟨lm2_1, lm2_2⟩
-        revert lm2_1
-        simp
-        intro t2_1 lm2_1 lm2_2
-        simp only [← lm2_1] at lm2_2
-        rewrite [embedAt_liftAt_eq] at lm2_2
-        subst lm2_2
--/
-/-
-        split
-        · rename_i t2 lm4
-          rewrite [liftAt?_eq_some_iff] at lm4
-          rcases lm4 with ⟨lm4_1, lm4_2⟩
-          revert lm4_1
-          simp
-          intro t2_1 lm4_1 lm4_2
-          rewrite [liftAt?_eq_none_iff] at lm2
--/
+    · refine Or.inl ?_
+      exists lv
+    · refine Or.inr ?_
+      exists lv
+  · intro lm1
+    rcases lm1 with lm1 | lm1
+    <;> (
+      rcases lm1 with ⟨lv, lm1, lm2⟩
+      replace lm2 := lm2.symm
+      subst lm2
+      simp [embedAt_liftAt_eq, lm1] )
 
-    --· dsimp [hunionIndicator]
-    --  split
 
-      --dsimp [hunionIndicator]
-      --simp at
-/-
-      split
-      · rename_i t1_2 lm3
-        rewrite [liftAt?_eq_some_iff] at lm3
-        rcases lm3 with ⟨lm3_1, lm3_2⟩
-        simp at lm1
--/
+
+
+
 
 /-
 theorem hunionIndicator_eq_true [HasPartialLift T1 T2 T3] {s1: T1 → Bool} {s2: T2 → Bool}
